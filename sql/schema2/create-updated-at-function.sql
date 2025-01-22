2efaa233f561b5e4e8c95ba8378821a3adf96fc7
huatzhi <huatzhi@gmail.com>
2025-01-22 09:33:05 +0800

    test fix

--- sql/schema2/create-updated-at-function.sql
+++ sql/schema2/create-updated-at-function.sql
@@ -1,7 +1,7 @@
 CREATE OR REPLACE FUNCTION schema2.update_updated_at()
-RETURNS TRIGGER AS $$
+RETURNS TRIGGER AS $function$
 BEGIN
     NEW.updated_at = CURRENT_TIMESTAMP;
     RETURN NEW;
 END;
-$$ language 'plpgsql';
+$function$ language 'plpgsql';
